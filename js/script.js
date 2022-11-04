d34ad82728d4e4c8709ff17d3a8f32c8d837909e
Konstanty Wrona <koswro@gmail.com>
2022-11-04 20:20:01 +0100

    Add logic to the game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let computerMove = 'nieznany ruch';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+if(randomNumber == 1 ){
+	computerMove = 'kamień';
+}	else if( randomNumber == 2){
+		computerMove = 'papier'
+	}
+	else{
+		computerMove = 'nożyce'
+	}
 
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1:kamień, 2:papier, 3:nożyce.');
+
+console.log ('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+	playerMove = 'kamień';
+}	else if( playerInput == '2'){
+		playerMove = 'papier'
+	}
+	else{
+		playerMove = 'nożyce'
+	}
+
+printMessage('Twój ruch to: ' + playerMove);
